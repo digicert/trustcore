2429060a4b2a9b51d7956d924f5bcbe80f974844
Prashant Kawale <prashant.kawale@digicert.com>
2026-04-16 07:27:16 +0000

    Update copyright information in common directory

--- src/common/vlong_const.h
+++ src/common/vlong_const.h
@@ -2,8 +2,17 @@
  * @file  vlong_const.h
  * @brief Very Long Integer Library Constant Time Operatons Header
  *
- * Copyright Digicert 2023. All Rights Reserved.
- * Proprietary and Confidential Material.
+ * Copyright 2026 DigiCert, Inc. All Rights Reserved.
+ *
+ * DigiCert® TrustCore SDK and TrustEdge are licensed under a dual-license model:
+ *
+ * 1. **Open Source License**: GNU Affero General Public License v3.0 (AGPL v3).
+ * See: https://github.com/digicert/trustcore/blob/main/LICENSE.md
+ * 2. **Commercial License**: Available under DigiCert's Master Services Agreement.
+ * See: https://www.digicert.com/master-services-agreement/
+ *
+ * *Use of TrustCore SDK or TrustEdge outside the scope of AGPL v3 requires a commercial license.*
+ * *Contact DigiCert at sales@digicert.com for more details.*
  */
 
 #ifndef __VLONG_CONST_HEADER__
