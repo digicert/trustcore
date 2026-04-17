dcebe39e538aee26b673ee07e6c3f00837f7200a
Prashant Kawale <prashant.kawale@digicert.com>
2026-04-17 07:22:36 +0000

    data est harness http mqtt ocsp platform

--- src/est/testunit/test_est_client_api.c
+++ src/est/testunit/test_est_client_api.c
@@ -3,7 +3,17 @@
  *
  * Unit tests for EST Client API functions
  *
- * Copyright Digicert 2026 All Rights Reserved.
+ * Copyright 2026 DigiCert, Inc. All Rights Reserved.
+ *
+ * DigiCert® TrustCore SDK and TrustEdge are licensed under a dual-license model:
+ *
+ * 1. **Open Source License**: GNU Affero General Public License v3.0 (AGPL v3).
+ * See: https://github.com/digicert/trustcore/blob/main/LICENSE.md
+ * 2. **Commercial License**: Available under DigiCert's Master Services Agreement.
+ * See: https://www.digicert.com/master-services-agreement/
+ *
+ * *Use of TrustCore SDK or TrustEdge outside the scope of AGPL v3 requires a commercial license.*
+ * *Contact DigiCert at sales@digicert.com for more details.*
  */
 
 #include <stdarg.h>
