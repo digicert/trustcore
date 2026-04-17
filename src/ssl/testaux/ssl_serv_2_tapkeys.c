6d06c46c79e60cb0e83e2885a7014b6d979a9323
Prashant Kawale <prashant.kawale@digicert.com>
2026-04-17 14:49:30 +0000

    Update copyright information in ssh, ssl, tap

--- src/ssl/testaux/ssl_serv_2_tapkeys.c
+++ src/ssl/testaux/ssl_serv_2_tapkeys.c
@@ -1,15 +1,17 @@
  /*
  * Implementation of a secure HTTP server
  *
- * Copyright 2025 DigiCert Project Authors. All Rights Reserved.
- * 
- * DigiCert® TrustCore and TrustEdge are licensed under a dual-license model:
- * - **Open Source License**: GNU AGPL v3. See: https://github.com/digicert/trustcore-test/blob/main/LICENSE
- * - **Commercial License**: Available under DigiCert’s Master Services Agreement. See: https://github.com/digicert/trustcore-test/blob/main/LICENSE_COMMERCIAL.txt  
- *   or https://www.digicert.com/master-services-agreement/
- * 
- * *For commercial licensing, contact DigiCert at sales@digicert.com.*
+ * Copyright 2026 DigiCert, Inc. All Rights Reserved.
  *
+ * DigiCert® TrustCore SDK and TrustEdge are licensed under a dual-license model:
+ *
+ * 1. **Open Source License**: GNU Affero General Public License v3.0 (AGPL v3).
+ * See: https://github.com/digicert/trustcore/blob/main/LICENSE.md
+ * 2. **Commercial License**: Available under DigiCert's Master Services Agreement.
+ * See: https://www.digicert.com/master-services-agreement/
+ *
+ * *Use of TrustCore SDK or TrustEdge outside the scope of AGPL v3 requires a commercial license.*
+ * *Contact DigiCert at sales@digicert.com for more details.*
  * This sample server is built similar to src/examples/ssl_example.c;
  * This example takes 2 keys and certs and populates the keystore with them.
  * This is mainly used to test the TAP local usecase with mutlitple TAP Keys
